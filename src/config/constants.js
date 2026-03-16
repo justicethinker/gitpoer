91c0c309ce7b1f52570c31bf0ba3215e8307016d
Justice Thinker <justicethinker2@gmail.com>
2026-03-16 16:38:15 +0100

    Delete src directory

--- src/config/constants.js
+++
@@ -1,3 +0,0 @@
-export const GITHUB_API_BASE = "https://api.github.com";
-export const GITHUB_RAW_BASE = "https://raw.githubusercontent.com";
-export const ANTHROPIC_API_KEY = import.meta.env.VITE_ANTHROPIC_API_KEY;
